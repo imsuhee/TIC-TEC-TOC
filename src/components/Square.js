2297de917eea5e7ed70d992642851e933d087acd
imsuhee <dlatngml123@nate.com>
2023-10-03 22:49:56 +0900

    이미지 경로 수정

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./Square.css"
+
 export const Square = ({ onClick, value }) => {
-    const imageSource = value === "X" ? "./img/cat-icon1.png" : value === "O" ? "./img/cat-icon2.png" : null;
+    const imageSource = value === "X" ? "https://github.com/imsuhee/TIC-TEC-TOC/blob/main/public/img/cat-icon1.png?raw=true" : value === "O" ? "https://github.com/imsuhee/TIC-TEC-TOC/blob/main/public/img/cat-icon2.png?raw=true" : null;
 
     // value가 null인 경우 이미지를 표시하지 않음
     return (
